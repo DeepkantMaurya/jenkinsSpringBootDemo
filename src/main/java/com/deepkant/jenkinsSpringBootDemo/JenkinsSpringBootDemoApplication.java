a928797149b0075b33ed3cbde2ecae33ff3d0608
Deepkant Maurya <deepkant@dawnbit.com>
2020-09-29 20:53:31 +0530

    second commit

---
+++ src/main/java/com/deepkant/jenkinsSpringBootDemo/JenkinsSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.deepkant.jenkinsSpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepkant/jenkinsSpringBootDemo/JenkinsSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepkant.jenkinsSpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
